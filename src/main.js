fc07c3e488c37e9b7b52c46752d4fdf2261989c0
Zach Weintraub <zachweintraub@gmail.com>
2019-06-07 13:04:28 -0700

    display name plus address phone and accepting new patients info

--- src/main.js
+++ src/main.js
@@ -7,36 +7,47 @@ import { DocService } from './DocService';
 
 function parseLatLong(json) {
     let body = JSON.parse(json);
-    return `${body.results[0].locations[0].latLng.lat},` + `${body.results[0].locations[0].latLng.lng},` + '10';
+    return `${body.results[0].locations[0].latLng.lat},` + `${body.results[0].locations[0].latLng.lng},` + '100';
 }
 
 function parseDoc(object, index) {
-    return `<div id="result${index}">
-        <p class="doc-name">${object.data[index].profile.last_name}, ${object.data[index].profile.first_name} ${object.data[index].profile.middle_name}
-        </p>
-    </div>`;
+    function parseAddress(addressObject){
+        let addressOutput = "";
+        addressOutput += `${addressObject.street} `;
+        if(object.street2 != undefined) {
+            addressOutput += `${addressObject.street2} `;
+        }
+        addressOutput += `${addressObject.city}, ${addressObject.state} ${addressObject.zip}`;
+        return addressOutput;
+    }
+    let output = `<div id="result${index}"><p class="doc-name">${object.data[index].practices[0].name}</p><div class="more-info"><p class="address">Address: ${parseAddress(object.data[index].practices[0].visit_address)}</p><p class="phone">Tel: ${object.data[index].practices[0].phones[0].number}</p>`;
+    if(object.data[index].practices[0].accepts_new_patients){
+        output += `<p class="new-patients">This doctor is accepting new patients.</p>`;
+    } else {
+        output += `<p class="new-patients">This doctor is not accepting new patients.</p>`;
+    }
+    output += `</div></div>`;
+    return output;
 }
 
 $().ready(function(){
     $('#searchForm').submit(function(e){
         e.preventDefault();
+        $('#results').empty();
         let locationService = new LocService();
         let thisLoc = $('#location').val();
         locationService.getLatLong(thisLoc)
         .then(function(response){
             let location = parseLatLong(response);
-            console.log(location);
             let query = $('#query').val();
             let doctorService = new DocService();
             return doctorService.getDocs(query, location);
         })
         .then(function(response){
             let results = JSON.parse(response);
-            console.log(results);
             let resultsHtml = "";
             for(let i = 0; i < results.data.length; i++){
                 resultsHtml += parseDoc(results, i);
-                console.log(resultsHtml);
             }
             $('#results').append(resultsHtml);
         });
