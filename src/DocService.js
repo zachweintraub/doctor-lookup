904374d71d5121e7905a2302bc00342b864555b4
Zach Weintraub <zachweintraub@gmail.com>
2019-06-07 11:09:11 -0700

    implement doctor lookup api call

---
+++ src/DocService.js
@@ -0,0 +1,18 @@
+export class DocService {
+    getDocs(query, location) {
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=${location}&user_key=${process.env.DOC_KEY}`;
+            request.onload = function() {
+                if (this.status == 200) {
+                    resolve(request.response);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            };
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
+
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { LocService } from './LocService';
+import { DocService } from './DocService';
+
+function parseLatLong(json) {
+    let body = JSON.parse(json);
+    return body.results[0].locations[0].latLng;
+}
+
+function parseDoc(object, index) {
+
+}
 
 $().ready(function(){
-    let locationService = new LocService();
-    locationService.getLatLong('seattle')
-    .then(function(response){
-        let body = JSON.parse(response);
-        console.log(body.results[0].locations[0].latLng);
+    $('#searchForm').submit(function(e){
+        e.preventDefault();
+        let locationService = new LocService();
+        let thisLoc = $('#location').val();
+        locationService.getLatLong(thisLoc)
+        .then(function(response){
+            let location = parseLatLong(response);
+            let query = $('#query').val();
+            let doctorService = new DocService();
+            return doctorService.getDocs(query, location);
+        })
+        .then(function(response){
+            
+        });
     });
   
 });
